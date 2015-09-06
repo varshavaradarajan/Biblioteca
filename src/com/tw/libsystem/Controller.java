69cf2775a8212593a9e99648b2ee68065684077d
varshavs <varshavs@INvarshavs.local>
2015-09-06 14:09:16 +0530

    Added spec: Controller should delegate displaying welcome message to display.

--- src/com/tw/libsystem/Controller.java
+++ src/com/tw/libsystem/Controller.java
@@ -1,12 +1,15 @@
-//Job of the controller class is to delegate the vrious operations to be performed.
+//Job of the controller class is to delegate the various operations to be performed.
 package com.tw.libsystem;
 
 public class Controller {
 
     private Factory factory;
+    private Display display;
 
     public void run() {
         factory = createFactory();
+        display = factory.buildDisplay("Welcome to Biblioteca");
+        display.displayMessage();
     }
 
     public Factory createFactory()
--- test/com/tw/libsystem/ControllerTest.java
+++ test/com/tw/libsystem/ControllerTest.java
@@ -1,10 +1,28 @@
 package com.tw.libsystem;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ControllerTest {
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setOutputStream() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void clearOutputStream() {
+        System.setOut(System.out);
+    }
 
     @Test
     public void shouldCreateAFactoryForManufacturingDisplays() {
@@ -13,4 +31,10 @@ public class ControllerTest {
         assertEquals(Factory.class, controller.createFactory().getClass());
     }
 
+    @Test
+    public void shouldDelegateDisplayingOfWelcomeMessage() {
+        Controller controller = new Controller();
+        controller.run();
+        assertEquals("Welcome to Biblioteca\n", outputStream.toString());
+    }
 }
