ab2e4775d9a5e80113ef811f3068838ddd085f2d
varshavs <varshavs@INvarshavs.local>
2015-09-07 17:02:26 +0530

    Added spec: Library should return its contents.

---
+++ src/com/tw/libsystem/Library.java
@@ -0,0 +1,24 @@
+package com.tw.libsystem;
+
+import java.util.ArrayList;
+
+public class Library {
+
+    ArrayList<Book> bookList;
+
+    public Library() {
+        bookList = new ArrayList<>();
+        bookList.add(new Book("Crime And Punishment", "Fyodor Dostoevsky", 1866));
+        bookList.add(new Book("Wuthering Heights", "Emily Bronte", 1847));
+        bookList.add(new Book("Pragmatic Programmer", "Dave Thomas", 1999));
+    }
+
+    @Override
+    public String toString() {
+        String bookNames = "Book Name\t\t\t\tAuthor Name\t\t\t\tYear\t\t\t\t\n";
+        for(Book names : bookList) {
+            bookNames += names.toString() + "\n";
+        }
+        return bookNames;
+    }
+}
---
+++ test/com/tw/libsystem/LibraryTest.java
@@ -0,0 +1,16 @@
+package com.tw.libsystem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+
+    @Test
+    public void shouldReturnTheContentsOfLibrary() {
+        Library library = new Library();
+
+        assertEquals("Book Name\t\t\t\tAuthor Name\t\t\t\tYear\t\t\t\t\nCrime And Punishment\tFyodor Dostoevsky\t1866\t\nWuthering Heights\tEmily Bronte\t1847\t\nPragmatic Programmer\tDave Thomas\t1999\t\n", library.toString());
+    }
+
+}
