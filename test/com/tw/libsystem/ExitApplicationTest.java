6d440d66354751d78ce07c34cd499de7bb187cdf
varshavs <varshavs@INvarshavs.local>
2015-09-14 12:43:14 +0530

    Added system-rules jar to the classpath.

--- test/com/tw/libsystem/ExitApplicationTest.java
+++ test/com/tw/libsystem/ExitApplicationTest.java
@@ -1,15 +1,15 @@
 package com.tw.libsystem;
 
-//import org.junit.Rule;
+import org.junit.Rule;
 import org.junit.Test;
-//import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import static org.junit.Assert.*;
 
 public class ExitApplicationTest {
 
-//    @Rule
-//    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
     @Test
     public void shouldExitApplicationUponExecutionOfThisCommand() {
@@ -18,13 +18,11 @@ public class ExitApplicationTest {
         assertEquals("Thank You!\n", exitApplication.toString());
     }
 
-//    @Test
-//    public void shouldExitFromTheApplication() {
-//        ExitApplication exitApplication = new ExitApplication();
-//        exit.expectSystemExitWithStatus(0);
-//
-//        exitApplication.execute();
-//    }
+    @Test
+    public void shouldExitFromTheApplication() {
+        ExitApplication exitApplication = new ExitApplication();
+        exit.expectSystemExitWithStatus(0);
 
-
+        exitApplication.execute();
+    }
 }
