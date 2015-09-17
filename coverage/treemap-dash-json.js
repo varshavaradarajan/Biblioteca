0cd223bc1c66f6b7dec551a93a1d003e5c94a00a
varshavs <varshavs@INvarshavs.local>
2015-09-17 08:56:01 +0530

    Removed coverage from git.

--- coverage/treemap-dash-json.js
+++
@@ -1,7 +0,0 @@
-var treeMapJson = {"id":"Clover database Thu Sep 17 2015 08:51:18 IST0","name":"","data":{
-    "$area":440.0,"$color":93.181816,"title":
-    " 440 Elements, 93.2% Coverage"},"children":[{"id":"com.tw.libsystem0",
-      "name":"com.tw.libsystem","data":{"$area":440.0,"$color":93.181816,
-        "title":"com.tw.libsystem 440 Elements, 93.2% Coverage"},"children":[]}]}
-;
-processTreeMapDashJson (treeMapJson);
